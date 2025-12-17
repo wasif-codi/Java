553a8a88ab7cb37dbe20d8c92ad8f42beb8b5e7c
Wasif <107427013+wasif-codi@users.noreply.github.com>
2025-12-18 00:00:28 +0530

    Update Abstraction.java

--- oops/Abstraction.java
+++ oops/Abstraction.java
@@ -1,1 +1,9 @@
-
+Abstraction
+===========  
+Abstraction means hiding internal implementation and exposing only required functionality.
+  
+How Abstraction is Achieved in Java
+====================================
+  By using
+Abstract classes
+Interfaces  
